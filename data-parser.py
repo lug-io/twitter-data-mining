50dd3787fbce8c730deb6260ae224a4147c889d5
unknown <csengineer13@gmail.com>
2016-01-24 09:23:05 -0500

    adjusting plot height to adjust

--- data-parser.py
+++ data-parser.py
@@ -1,8 +1,15 @@
 import json
 import pandas as pd
 import matplotlib.pyplot as plt
+from matplotlib import rcParams
 
-tweets_data_path = 'twitter-data.txt'
+# Set matplot settings
+rcParams.update({'figure.autolayout': True})
+#plt.gcf().subplots_adjust(bottom=0.15) # gcf -> GetCurrentFigure
+#plt.gca().tight_layout() # gca -> GetCurrentAxis
+
+# Set target file
+tweets_data_path = 'data.txt'
 
 tweets_data = []
 tweets_file = open(tweets_data_path, "r")
@@ -35,4 +42,17 @@ ax.set_ylabel('Number of tweets', fontsize=15)
 ax.set_title('Top 5 Languages', fontsize=15, fontweight='bold')
 tweets_by_lang[:5].plot(ax=ax, kind='bar', color='red')
 
-fig.savefig('chart.png')
+fig.savefig('top-5-languages.png')
+
+## Chart for top 5 countries
+tweets_by_country = tweets['country'].value_counts()
+
+fig, ax = plt.subplots()
+ax.tick_params(axis='x', labelsize=15)
+ax.tick_params(axis='y', labelsize=10)
+ax.set_xlabel('Countries', fontsize=15)
+ax.set_ylabel('Number of tweets', fontsize=15)
+ax.set_title('Top 5 Countries', fontsize=15, fontweight='bold')
+tweets_by_country[:5].plot(ax=ax, kind='bar', color='blue')
+
+fig.savefig('top-5-countries.png')
